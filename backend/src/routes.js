2099cb9402dafd47df40a910542f1f00384b0526
danieljesus <daniel.j.rodrigues06@gmail.com>
2020-02-02 12:09:05 -0300

    backend first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express')
+const routes = Router()
+
+const multer = require('multer')
+const multerConfig = require('./config/multer')
+
+const BoxController = require('./controllers/BoxController')
+const FileController = require('./controllers/FileController')
+
+routes.post('/boxes', BoxController.store)
+routes.get('/boxes/:id', BoxController.index)
+
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store)
+
+module.exports = routes
---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
+
+app.use((req, res, next) => {
+    req.io = io
+
+    return next();
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
+
+mongoose.connect('mongodb+srv://daniel:devbox@cluster0-rsnzv.mongodb.net/DevBox?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const routes = require('./routes')
+app.use(routes)
+
+server.listen(3333, () => console.log("Running on 3333"))
